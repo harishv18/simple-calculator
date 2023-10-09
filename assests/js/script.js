7c84bf1b69944c0e58c9ab8911442994070fce80
Harish V <harish00411@gmail.com>
2023-10-09 13:00:22 +0530

    simple calculator

---
+++ assests/js/script.js
@@ -0,0 +1,24 @@
+function result() {
+  var num1 = document.getElementById('inputfield1').value;
+  var num2 = document.getElementById('inputfield2').value;
+  var op = document.getElementById('operator').value;
+
+  if (op == '+') {
+    var result = parseInt(num1) + parseInt(num2);
+  }
+  else if (op == '-') {
+    var result = parseInt(num1) - parseInt(num2);
+  }
+  else if (op == '*') {
+    var result = parseInt(num1) * parseInt(num2);
+  }
+  else if (op == '/') {
+    var result = parseInt(num1) / parseInt(num2);
+  }
+  else if (op == '%') {
+    var result = parseInt(num1) % parseInt(num2);
+  }
+
+  document.getElementById('demo').innerHTML = result;
+}
+
